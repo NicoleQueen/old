6d6b615a26194972d9c586493604fbc2657f1014
99NicoleQ <99nicolequeen@github.com>
2019-08-26 11:36:13 -0700

    try

---
+++ MyApplication/app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.android.myapplication;
+
+import android.graphics.Color;
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    private void immersive(){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT){
+            return;
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            //设置状态栏颜色透明
+            window.setStatusBarColor(Color.TRANSPARENT);
+
+            int visibility = window.getDecorView().getSystemUiVisibility();
+            //布局内容全屏展示
+            visibility |= View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
+            //隐藏虚拟导航栏
+            visibility |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+            //防止内容区域大小发生变化
+            visibility |= View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+
+            window.getDecorView().setSystemUiVisibility(visibility);
+        }else {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+
+    }
+
+}
